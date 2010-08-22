f5e6dd6c40da653de3364deb42ffcb0f5fcc8d97
JohnAlbin <virtually.johnalbin@gmail.com>
2010-08-23 02:47:45 +0800

    Add new single_blog_block_item theme hook.

---
+++ single-blog-block-item.tpl.php
@@ -0,0 +1,30 @@
+<?php
+// $Id: menu-block-wrapper.tpl.php,v 1.9 2010/03/26 21:03:29 johnalbin Exp $
+
+/**
+ * @file
+ * HTML for an item in the single blog's block listing.
+ *
+ * Available variables:
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $title: A renderable array that that provides a title and link to the node.
+ * - $name: Themed username of node author output from theme_username().
+ *
+ * The following variables are provided for contextual information.
+ * - $node: Partial node object. Contains data that may not be safe.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $uid: User ID of the node author.
+ *
+ * @see template_preprocess_single_blog_block_item()
+ */
+?>
+<div class="<?php print $classes; ?>">
+
+  <div class="date"><?php print $date; ?>:</div>
+
+  <h4<?php print $title_attributes; ?>><?php print render($title); ?></h4>
+
+  <div class="name"><?php print t('by !username', array('!username' => $name)); ?></div>
+
+</div>
