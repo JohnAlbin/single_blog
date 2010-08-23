235f5a492bd6959c0fa4c0cb63d18f4c14c275de
JohnAlbin <virtually.johnalbin@gmail.com>
2010-08-24 02:03:20 +0800

    Add docs for $classes and $classes_array variables.

--- single-blog-block-item.tpl.php
+++ single-blog-block-item.tpl.php
@@ -6,11 +6,20 @@
  * HTML for an item in the single blog's block listing.
  *
  * Available variables:
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - single-blog-block-item: The current template type, i.e., "theming hook".
  * - $date: Formatted creation date. Preprocess functions can reformat it by
  *   calling format_date() with the desired parameters on the $created variable.
  * - $title: A renderable array that that provides a title and link to the node.
  * - $name: Themed username of node author output from theme_username().
  *
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * Other variables:
  * The following variables are provided for contextual information.
  * - $node: Partial node object. Contains data that may not be safe.
  * - $created: Time the node was published formatted in Unix timestamp.
